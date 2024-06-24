05ba2985e17ea89c4a7463b850712db8d5ca1855
daniel komoe <daniel.komoe@epitech.eu>
2024-06-24 08:26:19 +0200

    [ADD] adding final netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-// astro.config.mjs
+
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify";
@@ -7,22 +7,16 @@ import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
-import { initNetlifyIdentity } from "./src/script/auth-netlify-identity.js";
 
-// Define the site configuration
-const siteConfig = {
+// https://astro.build/config
+export default defineConfig({
   site: 'https://daniel-komoe-dev.netlify.app', // Ensure your site URL is correctly set here
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: ['https://daniel-komoe-dev.netlify.app/sitemap.xml'],
     }),
-    solidJs({
-      async beforeBuild() {
-        // Initialize Netlify Identity before building
-        await initNetlifyIdentity();
-      }
-    }),
+    solidJs(),
     UnoCSS({ injectReset: true }),
     icon(),
     netlify(), // Add Netlify adapter for static site generation
@@ -31,36 +25,4 @@ const siteConfig = {
     remarkPlugins: [remarkReadingTime],
   },
   output: "static", // Generate static HTML files
-};
-
-export default defineConfig(siteConfig);
-
-
-//import { defineConfig } from "astro/config";
-//import sitemap from "@astrojs/sitemap";
-//import netlify from "@astrojs/netlify";
-//import robotsTxt from "astro-robots-txt";
-//import UnoCSS from "@unocss/astro";
-//import icon from "astro-icon";
-//import solidJs from "@astrojs/solid-js";
-//import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
-//
-//// https://astro.build/config
-//export default defineConfig({
-//  site: 'https://daniel-komoe-dev.netlify.app', // Ensure your site URL is correctly set here
-//  integrations: [
-//    sitemap(),
-//    robotsTxt({
-//      sitemap: ['https://daniel-komoe-dev.netlify.app/sitemap.xml'],
-//    }),
-//    solidJs(),
-//    UnoCSS({ injectReset: true }),
-//    icon(),
-//    netlify(), // Add Netlify adapter for static site generation
-//  ],
-//  markdown: {
-//    remarkPlugins: [remarkReadingTime],
-//  },
-//  output: "static", // Generate static HTML files
-//});
-//
+});
